b0be4026f460a19e6493907de615e95347c02329
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-06-11 15:11:41 +0900

    123123

--- Simple MapView/ViewController.swift
+++ Simple MapView/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     //pin 객체 저장
     var annotations = [MKPointAnnotation]()
+    var count = 0
     
     
     override func viewDidLoad() {
@@ -48,7 +49,9 @@ class ViewController: UIViewController {
                                  "부산광역시 부산진구 양정동 393-18",
                                  "부산광역시 부산진구 양정1동 356-22",
                                  "부산광역시 부산진구 양정1동 산19-8",
-                                 "부산광역시 부산진구 양정동 353-38"]
+                                 "부산광역시 부산진구 양정동 353-38",
+                                 "부산광역시 부산진구 양정동 429-19"]
+        var foodStoreNames = ["늘해랑","번개반점", "아딸", "왕짜장", "토마토도시락","동의과학대학교", "홍콩반점"]
         
         for addr in foodStoreAddress {
             let geoCoder = CLGeocoder()
@@ -65,11 +68,14 @@ class ViewController: UIViewController {
                     
                     let anno = MKPointAnnotation()
                     anno.coordinate = loc!
-                    anno.title = "wait!"
+                    anno.title = foodStoreNames[self.count]
+                    self.count = self.count + 1
                     anno.subtitle = addr
-//                    self.self.myMapView.addAnnotation(anno)
+                    //self.self.myMapView.addAnnotation(anno)
                     self.annotations.append(anno)
                     self.myMapView.addAnnotations(self.annotations)
+                    
+                    //여러개의 pin을 지도에 꽉채움
                     self.myMapView.showAnnotations(self.annotations, animated: true)
                     
                     
